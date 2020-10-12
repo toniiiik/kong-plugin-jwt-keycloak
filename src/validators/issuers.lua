ba1610480638cd9068bff4e489d4a34c7fd5438b
toniiiik <32296236+toniiiik@users.noreply.github.com>
2020-10-12 10:46:15 +0200

    INT: validate iss by regex if exactly one allowed issuers is defined

--- src/validators/issuers.lua
+++ src/validators/issuers.lua
@@ -5,6 +5,16 @@ local function validate_issuer(allowed_issuers, jwt_claims)
     if jwt_claims.iss == nil then
         return nil, "Missing issuer claim"
     end
+
+    -- if exactly one issuer it can be regular expression
+    if table.getn(allowed_issuers) == 1 do
+        local issuer = allowed_issuers[1]
+        if jwt_claims.iss.match(issuer) == jwt_claims.iss then
+            return true
+        end
+        return nil, "Token issuer not allowed"
+    end
+
     for _, curr_iss in pairs(allowed_issuers) do
         if curr_iss == jwt_claims.iss then
             return true
